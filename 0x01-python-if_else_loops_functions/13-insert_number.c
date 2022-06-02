ea4ccb924d6fc0cada0fd4592be1bcf8373bd0f1
JuliusDgenius <ibejulius1@gmail.com>
2022-06-02 09:48:32 +0100

    13-insert_number.c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -2,41 +2,41 @@
 #include "lists.h"
 
 /**
- * insert_node - inserts a number into a given sorted singly-linked list
+ * insert_node - Inserts a number into a given sorted singly-linked list
  * @head: The pointer to the head of the linked list
- * @number: number to insert
+ * @number: The number to insert
  *
- * Return: address of the new node, or NULL if it failed
+ * Return: The address of the new node, or NULL if it failed
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *cur_node = NULL, *node = NULL;
+listint_t *cur_node = NULL, *node = NULL;
 
-	if (head != NULL)
-	{
-		node = malloc(sizeof(listint_t));
-		if (node != NULL)
-		{
-			node->n number;
-			cur_node = *head;
-			while ((cur_node != NULL) && (cur_node->n < number))
-			{
-				if (((cur_node->next != NULL) && (cur_node->next->n < number)))
-					cur_node = cur_node->next;
-				else
-					break;
-			}
-			if ((cur_node != NULL) && (cur_node->n < number))
-			{
-				node->next = cur_node->next;
-				cur_node->next = node;
-			}
-			else
-			{
-				node->next = *head;
-				*head = node;
-			}
-		}
-	}
-	return (node);
+if (head != NULL)
+{
+node = malloc(sizeof(listint_t));
+if (node != NULL)
+{
+node->n = number;
+cur_node = *head;
+while ((cur_node != NULL) && (cur_node->n < number))
+{
+if (((cur_node->next != NULL) && (cur_node->next->n < number)))
+cur_node = cur_node->next;
+else
+break;
+																					}
+if ((cur_node != NULL) && (cur_node->n < number))
+{
+node->next = cur_node->next;
+cur_node->next = node;
+																					}
+																					else
+																					{
+node->next = *head;
+*head = node;
+}
+}
+}
+return (node);
 }
