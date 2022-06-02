96346fafadf6dea385dafefe300e14bdaa87ca92
JuliusDgenius <ibejulius1@gmail.com>
2022-06-02 07:31:26 +0100

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,7 +5,7 @@ def fizzbuzz():
     but for multiples of 3 prints 'Fizz' and multiples of 5 
     prints 'Buzz'
     """
-    for i in range(1, 100):
+    for i in range(1, 101):
         print('{:s}{:s}{:s}'.format(
             str(i) * ((i % 3 != 0) and (i % 5 != 0)),
             'Fizz' * (i % 3 == 0),
