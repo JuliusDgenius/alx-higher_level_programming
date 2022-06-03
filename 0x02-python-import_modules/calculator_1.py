f5a3225abca6e2a1471acff37db973d8bbbc7fe3
JuliusDgenius <ibejulius1@gmail.com>
2022-06-03 03:27:02 +0100

    Tasks

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Addition function
+    Args:
+    a: first int
+    b: second int
+
+    Returns:
+    The value of a + b
+    """
+    return (a + b)
+
+def sub(a, b):
+    """My subtraction function
+    Args:
+    a: first integer
+    b: second it
+
+    Returns:
+    returns the value of a - b
+    """
+    return (a - b)
+
+def div(a, b):
+    """Division function
+    Args:
+    a: first num
+    b: second num
+
+    Returns:
+    the value of a / b
+    """
+    return int(a / b)
+
+def mul(a, b):
+    """Multiplication function
+    Args:
+    a: first int
+    b: second int
+
+    Returns:
+    the value of a * b
+    """
+
+    return (a * b)
