b4f63871cae09d90579001614ead9ee7141874f3
JuliusDgenius <ibejulius1@gmail.com>
2022-06-07 18:22:58 +0100

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,9 +2,5 @@
 
 
 def print_list_integer(my_list=[]):
-    i = 0
-    for i in range(len(my_list)):
-            if my_list[i] == "H":
-                continue
-            else:
-                print("{:d}\n".format(my_list[i]))
+    for n in my_list:
+        print("{:d}".format(n))
