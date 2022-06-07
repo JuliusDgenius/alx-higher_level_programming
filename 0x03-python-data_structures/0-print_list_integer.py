c92737ec433fbf10067c51bef171df5a679c92dc
JuliusDgenius <ibejulius1@gmail.com>
2022-06-07 17:50:37 +0100

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,8 +3,5 @@
 
 def print_list_integer(my_list=[]):
     i = 0
-    if my_list == "":
-        return ("No elements found")
-    else:
-        for i in range(len(my_list)):
-            print("{:d}\n".format(my_list[i]))
+    for i in range(len(my_list)):
+        print("{:d}\n".format(my_list[i]))
