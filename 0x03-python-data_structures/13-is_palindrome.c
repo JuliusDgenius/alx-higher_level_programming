db6dbf2b359a1415ff85a2952bc42f39518e2fd2
JuliusDgenius <ibejulius1@gmail.com>
2022-06-08 03:55:38 +0100

    13-is_palindrome.c

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,20 @@
+#include "list.h"
+
+/**
+ * is_palindrome - check id=f a linked list is a palindrome
+ * @head: pointer to head of first list
+ * Return: 0 or 1
+ */
+int is_palindrome(listint_t **head)
+{
+	const listint_t *current;
+	const listint_t *tail;
+	unsigned int n = 0;
+
+	//Get length
+	//Use a func to get a node at a specific index
+	//check the first to last and second to second last ...
+	//return true at the end (if all passed the checks)
+	
+	return (n);
+}
