7195ca616d51de8717a8627bf8593a79a8c8e3f4
JuliusDgenius <ibejulius1@gmail.com>
2022-06-21 05:54:14 +0100

    python exception

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    value = 0
+    for idx in range(x):
+        try:
+            print("{:d}".format(my_list[idx]), end="")
+            value += 1
+        except IndexError:
+            break
+        except Excepton:
+            pass
+
+        print("")
+        return (value)
