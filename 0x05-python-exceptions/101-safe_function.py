07745f6c98c982f85eb6828710fe58c49416d657
JuliusDgenius <ibejulius1@gmail.com>
2022-06-21 06:35:46 +0100

    python exception

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -2,6 +2,7 @@
 
 import sys
 
+
 def safe_function(fct, *args):
     """Executes a function safely.
     Args:
@@ -14,6 +15,6 @@ def safe_function(fct, *args):
     try:
         result = fct(*args)
         return (result)
-    except:
+    except Exception as exception:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
