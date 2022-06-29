a48009eb14796def903ada54130cc9003e1b5c17
JuliusDgenius <ibejulius1@gmail.com>
2022-06-29 04:24:22 +0100

    2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a rectangle class."""
 
+
 class Rectangle:
     """Represent a rectangle."""
 
@@ -48,4 +49,3 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
-
