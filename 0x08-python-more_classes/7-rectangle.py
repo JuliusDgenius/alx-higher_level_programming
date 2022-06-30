fef0e087f3b81034a854ef4f098ecc32a7d79cc6
JuliusDgenius <ibejulius1@gmail.com>
2022-06-30 03:45:50 +0100

    7-rectangle.py

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -1,4 +1,5 @@
-#!/usr/python3
+#!/usr/bin/python3
+# 7-rectangle.py
 """Defines a Rectangle class."""
 
 
@@ -15,8 +16,8 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
         Args:
-            width (int): the width of the new rectangle
-            height (int): the height of the new rectangle,
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
         """
         type(self).number_of_instances += 1
         self.width = width
@@ -37,7 +38,7 @@ class Rectangle:
 
     @property
     def height(self):
-        """Get/set the height of the rectangle"""
+        """Get/set the height of the Rectangle."""
         return self.__height
 
     @height.setter
@@ -59,8 +60,8 @@ class Rectangle:
         return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
-        """Returns a printablr of the rectangle.
-        Represent the rectangle with the '#' char
+        """Return the printable representation of the Rectangle.
+        Represents the rectangle with the # character.
         """
         if self.__width == 0 or self.__height == 0:
             return ("")
