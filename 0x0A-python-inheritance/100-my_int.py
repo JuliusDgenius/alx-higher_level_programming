cb532ab38e4add3cbd445bacb6e32abf5a6be061
JuliusDgenius <ibejulius1@gmail.com>
2022-07-04 18:57:20 +0100

    Tasks

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,7 +5,6 @@
 class MyInt(int):
     """Invert int operators == and !=."""
 
-
     def __eq__(self, value):
         """Override == operator with != behaviour"""
         return self.real != value
