c04607d4288e134efccdff929a972dc1f703d5f0
JuliusDgenius <ibejulius1@gmail.com>
2023-08-13 20:34:47 +0100

    correction

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,8 +2,10 @@
 
 
 def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list):
+
+    if (idx < 0 or idx > len(my_list)):
         return None
-    for i in my_list:
-        if i == idx:
-            return my_list[i]
+    else:
+        for i in my_list:
+            if i == idx:
+                return my_list[i]
