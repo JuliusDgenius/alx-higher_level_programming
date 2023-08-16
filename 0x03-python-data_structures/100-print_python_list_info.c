dbc8e44fc63dda471eaf207a77a986f80d4ec5ac
JuliusDgenius <ibejulius1@gmail.com>
2023-08-16 16:56:56 +0100

    Task 14

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,4 +1,5 @@
-
+#include <stdio.h>
+#include <Python.h>
 
 /**
  * print_python_list_info - Function prints basic info about Python lists.
@@ -17,7 +18,7 @@ void print_python_list_info(PyObject *p)
 
     for (i = 0; i < size; i++)
     {
-        printf("[*] Size of the {ython List = %d\n", size);
+        printf("[*] Size of the Python List = %d\n", size);
         printf("[*] Allocated = %d\n", allocate);
 
         for (int i = 0; i < size; i++)
