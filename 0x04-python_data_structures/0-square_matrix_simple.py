08348cb2ed97addfaeeeb0de54bc3635cfe9c2c1
Julius Okwe <85571573+JuliusDgenius@users.noreply.github.com>
2023-08-17 14:52:36 +0100

    Delete 0x04-python_data_structures directory

--- 0x04-python_data_structures/0-square_matrix_simple.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-
-def square_matrix_simple(matrix=[]):
-    squared_matrix = []
-    for row in matrix:
-        squared_matrix.append([i ** 2 for i in row])
-    return squared_matrix
--- 0x04-python_data_structures/102-complex_delete.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def complex_delete(my_dict, value):
-    tmp = my_dict.copy
-    for j, k in tmp.items():
-        if value == k:
-            my_dict.pop(k)
-    return my_dict
