878bd56884ae43079877c752eeff943db5fe1d44
JuliusDgenius <ibejulius1@gmail.com>
2023-08-29 23:17:43 +0100

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Square class with a private size attribute."""
+
+
+class Square:
+    """Represents square w/ a private size attribute."""
+    def __init__(self, __size):
+        self.__size = __size
