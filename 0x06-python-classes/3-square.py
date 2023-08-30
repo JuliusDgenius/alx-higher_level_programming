cfa211fd47e63c79c558caa1500606df6428afa5
JuliusDgenius <ibejulius1@gmail.com>
2023-08-30 13:42:49 +0100

    Task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Define Square class"""
+
+
+class Square:
+    """Represents a square"""
+    def __init__(self, size=0):
+        """Initializes a square object"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """computes the area of a square"""
+        return self.__size * self.__size
