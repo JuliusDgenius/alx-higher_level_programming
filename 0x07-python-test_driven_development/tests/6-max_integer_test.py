d91315b5e18311e8996b09d65e6bd32e7148a27b
JuliusDgenius <ibejulius1@gmail.com>
2023-09-03 04:17:58 +0100

    tests/6-max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,13 +1,41 @@
-"""This module is the test suites fot the 'max_integer' function
-to return the maximum integer in a list
+#!/usr/bin/python3
+"""Unittests for max_integer
 """
-# import the unittest module and the function
+
 import unittest
+import random
+
+
 max_integer = __import__('6-max_integer').max_integer
 
 
-class testMaxInteger(unittest.TestCase):
-    """The class for testing max_integer function"""
-    def test_list_empty(self):
-        # Check if the list is empty
-        self.assertIsNone(max_integer([]))
+class TestMaxInteger(unittest.TestCase):
+    """Define unit test for the max_integer function"""
+
+    def test_ordered_list(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_max_in_the_middle(self):
+        self.assertEqual(max_integer([1,2,3,7,4,5,6]), 7)
+
+    def test_unordered_list(self):
+        self.assertEqual(max_integer([4, 2, 3, 1]), 4)
+
+    def test_empty_list(self):
+        self.assertEqual(max_integer([]), None)
+
+    def test_one_element_list(self):
+        self.assertEqual(max_integer([5]), 5)
+
+    def test_floats(self):
+        self.assertEqual(max_integer([1.5, 1.7, 2.8]), 2.8)
+
+    def test_string_implicit_ansii_values(self):
+        self.assertEqual(max_integer('abcd'), 'd')
+
+    def test_empty_string(self):
+        self.assertEqual(max_integer(''), None)
+
+
+                                                                            if __name__ == '__main__':
+    unittest.main()
