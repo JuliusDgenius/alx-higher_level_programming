b0ca1690a9c23c44d38d720621f097a16a2d4055
JuliusDgenius <ibejulius1@gmail.com>
2023-09-06 14:03:41 +0100

    Task 30

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,9 @@
+"""This module defines a locked class, LockedClass"""
+
+
+class LockedClass:
+    """A locked class that prevents the user from dynamaically
+    creating new instance attribute, except first_name
+    """
+
+    __slots__ = ["first_name"]
