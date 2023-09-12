67a1439edba84900e71d92a0786f65d23eb4dfba
JuliusDgenius <ibejulius1@gmail.com>
2023-09-12 06:58:13 +0100

    Task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module containing is_same_class function"""
+
+
+def is_same_class(obj, a_class):
+    """return True id object is exactly an instance of a class.
+    Args:
+        obj (object): class instance to check.
+        a_class (class): The class to check obj with
+    """
+    if (isinstance(obj, a_class)):
+        return True
+    else:
+        return False
