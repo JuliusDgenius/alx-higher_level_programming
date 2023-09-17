d81a805bb17b613be75c38add0ccefa12c620583
JuliusDgenius <ibejulius1@gmail.com>
2023-09-17 17:29:50 +0100

    test 6

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -36,6 +36,5 @@ class TestMaxInteger(unittest.TestCase):
     def test_empty_string(self):
         self.assertEqual(max_integer(''), None)
 
-
-                                                                            if __name__ == '__main__':
+if __name__ == '__main__':
     unittest.main()
