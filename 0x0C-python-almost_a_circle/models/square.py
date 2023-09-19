f4931f13b98ebfb20bf5b48c53ece890f39f68c2
JuliusDgenius <ibejulius1@gmail.com>
2023-09-19 20:55:40 +0100

    updates

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -29,3 +29,29 @@ class Square(Rectangle):
         """Sets size"""
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """updates multiple attributes"""
+        if len(args):
+            for i, a in enumerate(args):
+                if i == 0:
+                    self.id = a
+                elif i == 1:
+                    self.size = a
+                elif i == 2:
+                    self.x = a
+                elif i == 3:
+                    self.y = a
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+                if "size" in kwargs:
+                    self.width = kwargs["size"]
+                    if "x" in kwargs:
+                        self.x = kwargs["x"]
+                        if "y" in kwargs:
+                            self.y = kwargs["y"]
+
+    def to_dictionary(self):
+        """Return the dictionary representation of the Square"""
+        return ({"id": self.id, "size": self.width, "x": self.x, "y": self.y})
