7f5c9b174492eb92afd10605ef5e3c36752a7b00
JuliusDgenius <ibejulius1@gmail.com>
2023-09-19 10:26:47 +0100

    implement __str__ method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -109,3 +109,7 @@ class Rectangle(Base):
             [print(" ", end="") for x in range(0, self.x)]
             [print("#", end="") for w in range(0, self.width)]
             print()
+
+    def __str__(self):
+        """Returns a string representation of a Rectangle"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
