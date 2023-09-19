6efff18a0736eb6af885a3f3fd8c4ecc43ab71c4
JuliusDgenius <ibejulius1@gmail.com>
2023-09-19 12:06:56 +0100

    implement update method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -115,7 +115,7 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y,
                                                        self.width, self.height)
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """Assigns an argument to each attribute"""
         for arg in args:
             if len(args) == 1:
@@ -128,3 +128,19 @@ class Rectangle(Base):
                 self.__x = arg
             elif len(args) == 5:
                 self.__y = arg
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = v
+                    elif k == "width":
+                        self.width = v
+                    elif k == "height":
+                        self.height = v
+                    elif k == "x":
+                        self.x = v
+                    elif k == "y":
+                        self.y = v
