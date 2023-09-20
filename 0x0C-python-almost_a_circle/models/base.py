8c5208f7c4bb1a56b380cf303e46a6f191bd9d57
JuliusDgenius <ibejulius1@gmail.com>
2023-09-20 03:06:26 +0100

    implemented to_json method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,6 +24,6 @@ class Base:
         Args:
             list_dictionaries (list): A list dictionary
         """
-        if list_dictionaries is None or list_dictionaries = []:
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
