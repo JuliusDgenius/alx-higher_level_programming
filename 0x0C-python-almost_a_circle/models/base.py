5533580df9319f8f139f1dbdf22d1e975bb46084
JuliusDgenius <ibejulius1@gmail.com>
2023-09-20 04:32:37 +0100

    **dictionary

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -56,3 +56,17 @@ class Base:
         if json_string is None or json_string == []:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all the attributes set already.
+        Args:
+            dictionary (dict): A double pointer to a dictionary
+        """
+        if (dictionary):
+            if cls.__name__ == 'Rectangle':
+                new = cls(1, 1)
+            else:
+                new = cls(1)
+                new.update(**dictionary)
+                return new
