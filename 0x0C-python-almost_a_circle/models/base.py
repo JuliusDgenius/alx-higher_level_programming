062bb3d1d848f8af4fc18c024fc7f73dae3ba7bb
JuliusDgenius <ibejulius1@gmail.com>
2023-09-20 11:29:45 +0100

    create dictionary

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -63,13 +63,13 @@ class Base:
         Args:
             dictionary (dict): A double pointer to a dictionary
         """
-        if (dictionary):
-            if cls.__name__ == 'Rectangle':
-                new = cls(1, 1)
-            else:
-                new = cls(1)
-            new.update(**dictionary)
-        return new
+      if (dictionary):
+          if cls.__name__ == 'Rectangle':
+              new_rect = cls(1, 1)
+          else:
+              new_rect = cls(1)
+          new_rect.update(**dictionary)
+      return new_rect
 
     @classmethod
     def load_from_file(cls):
