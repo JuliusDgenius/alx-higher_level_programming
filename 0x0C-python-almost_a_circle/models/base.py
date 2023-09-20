07f30e88c4bae862ebc7e408e1134e8396ff7b46
JuliusDgenius <ibejulius1@gmail.com>
2023-09-20 11:13:32 +0100

    use **kwargs

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -63,13 +63,13 @@ class Base:
         Args:
             dictionary (dict): A double pointer to a dictionary
         """
-       if (dictionary):
-           if cls.__name__ == 'Rectangle':
-               new = cls(1, 1)
-           else:
-               new = cls(1)
-               new.update(**dictionary)
-       return new
+        if (dictionary):
+            if cls.__name__ == 'Rectangle':
+                new = cls(1, 1)
+            else:
+                new = cls(1)
+                new.update(**dictionary)
+        return new
 
     @classmethod
     def load_from_file(cls):
