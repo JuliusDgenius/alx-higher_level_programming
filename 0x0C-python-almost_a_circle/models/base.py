ebf4ee676ac39d0bfaf5789a08e1bdb460f51b2b
JuliusDgenius <ibejulius1@gmail.com>
2023-09-20 03:47:58 +0100

    save to string method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,3 +28,21 @@ class Base:
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """A class method that writes the JSON string represemtation
+        of list_obj to a file
+        Args:
+            list_objs (list): A list of a class instance objects
+        """
+        filename = cls.__name__ + ".json"
+        empty_list = []
+        with open(filename, "w") as json_file:
+            if list_objs is None:
+                return json_file.write("[]")
+            else:
+                for list_obj in list_objs:
+                    empty_list = [list_obj.to_dictionary() for list_obj
+                                  in list_objs]
+                json_file.write(Base.to_json_string(empty_list))
