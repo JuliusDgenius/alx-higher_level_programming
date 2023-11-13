50303e0d220d3ba599c74c50d3bfdc9eb1311027
Julius Ibe <ibejulius1@gmail.com>
2023-11-13 16:40:05 +0100

    Task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const myvar = 'C is fun';
+const myVar1 = 'Python is cool';
+const myVar2 = 'Javascript is amazing';
+console.log(myvar);
+console.log(myVar1);
+console.log(myVar2);
