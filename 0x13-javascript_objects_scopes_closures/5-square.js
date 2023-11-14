43064c75636e3376c8b6112560e25285ccd85824
JuliusDgenius <ibejulius1@gmail.com>
2023-11-14 16:09:26 +0100

    Task

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,9 @@
+#!/bin/node
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+module.exports = Rectangle;
+module.exports = Square;
