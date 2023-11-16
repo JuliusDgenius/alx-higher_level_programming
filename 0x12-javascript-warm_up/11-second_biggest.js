e7c4bd83d96d5f373e4d8765f0a6fb2965376f64
JuliusDgenius <ibejulius1@gmail.com>
2023-11-16 13:25:46 +0100

    Task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@ if (myVar <= 3) {
   console.log(0);
 } else {
   const args = process.argv.slice(2).map(x => parseInt(x));
-  const sorted = args.sort();
+  const sorted = args.sort((a, b) => a - b);
   const lenSorted = sorted.length;
   console.log(sorted[lenSorted - 2]);
 }
