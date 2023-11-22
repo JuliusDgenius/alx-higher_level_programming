b89b9e472ea64776c912e3af11c428fa2e73c920
JuliusDgenius <ibejulius1@gmail.com>
2023-11-22 07:44:46 +0100

    Task

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-# Displays all cities of a given state from the states table of the
-# database hbtn_0e_4_usa.
-# Safe from SQL injections.
-# Usage: ./5-filter_cities.py <mysql username> \
-#                             <mysql password> \
-#                             <database name> \
-#                             <state namse searched>
+"""Displays all cities of a given state from the state table"""
 import sys
 import MySQLdb
 
