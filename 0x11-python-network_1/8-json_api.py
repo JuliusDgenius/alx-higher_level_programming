005ba812e26d62fe05583a2b5b4d1f97fe79dd68
JuliusDgenius <ibejulius1@gmail.com>
2023-12-04 14:43:30 +0100

    Task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""A python script that takes ina letter and sends a POST request to
+    'http://0.0.0.0:5000/search_user' with the letter as parameter.
+"""
+import sys
+import requests
+
+
+if __name__ == '__main__':
+    url = 'http://0.0.0.0:5000/search_user'
+    l = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": l}
+
+    r = requests.post(url, data=payload)
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print(Not a valid JSON)
