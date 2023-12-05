5029a434ce9bef4ddb857a2943d6e240501c6d80
JuliusDgenius <ibejulius1@gmail.com>
2023-12-05 17:55:20 +0100

    Task

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Python script that takes 2 args in order to list 10 commits
+    (from the most recent to the oldest) of the repository "rails"
+    by the user "rails"
+"""
+import sys
+import requests
+
+
+if __name__ == '__main__':
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    url =  'https://api.github.com/repos/owner_name/repo_name/commits'
+    r = requests.get(url)
+    commits = r.json()
+    print(commits)
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
