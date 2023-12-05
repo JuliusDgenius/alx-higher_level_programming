9478e8e7d542c0b37a373e2d26f4c492ea3f0fee
JuliusDgenius <ibejulius1@gmail.com>
2023-12-05 18:10:03 +0100

    Task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,8 +10,8 @@ import urllib.request
 
 if __name__ == '__main__':
     url = sys.argv[1]
-    email = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(email).encode("ascii")
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
 
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
