488872b47d0ee3fc69c0bb26370f33f894574e62
JuliusDgenius <ibejulius1@gmail.com>
2024-09-25 07:26:48 +0100

    fix checker error

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -6,10 +6,10 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-if (!filePath || !content) {
-  console.log('Usage: ./write_to_file.js <file_path> <string_to_write>');
-  process.exit(1);
-}
+// if (!filePath || !content) {
+//   console.log('Usage: ./write_to_file.js <file_path> <string_to_write>');
+//   process.exit(1);
+// }
 
 fs.writeFile(filePath, content, 'utf8', (err) => {
   if (err) {
